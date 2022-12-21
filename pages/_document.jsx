b2d0dd9e4902a0d4da8725efb8c714ce99373388
rkrobby19 <rizkyrobby19@gmail.com>
2022-12-21 09:57:29 +0700

    Add prettier script

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,6 +1,4 @@
-import {
-  Html, Head, Main, NextScript,
-} from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
 
 export default function Document() {
---
+++ pages/login.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import React from 'react';
+
+function Login() {
+  return (
+    <>
+      <Head>
+        <title>Login Page</title>
+      </Head>
+      <main>
+        <h1>Login Page</h1>
+      </main>
+    </>
+  );
+}
+
+export default Login;
